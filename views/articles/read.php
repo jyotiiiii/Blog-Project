343f51d9d76a9fb197a33d0c76211e2cc2c0ba13
Gabriella <Gabriella@192.168.0.14>
2019-04-19 17:38:22 +0100

    Create functionality in article

--- views/articles/read.php
+++ views/articles/read.php
@@ -2,22 +2,22 @@
 
 
 <p>Headline: <?php echo $article->headline; ?></p>
-<p>Author: <?php echo $article->first_name; ?> <?php echo $article->first_name; ?> </p>
-<p>Tags: <?php echo $article->tag_name; ?></p>
+<p>Author: <?php// echo $article->first_name; ?> <?php //echo $article->first_name; ?> </p>
+<p>Tags: <?php //echo $article->tag_name; ?></p>
 <p><?php echo $article->text; ?></p>
 <p>Tags: <?php echo $article->tag_name; ?></p>
 <?php 
 
-//Need to point to pinterest url or file location
-$file = 'views/images/' . $article->headline . '.jpeg';
-if(file_exists($file)){
-    $img = "<img src='$file' width='150' />";
-    echo $img;
-}
-else
-{
-echo "<img src='views/images/standard/_noproductimage.png' width='150' />";
-}
+////Need to point to pinterest url or file location
+//$file = 'views/images/' . $article->headline . '.jpeg';
+//if(file_exists($file)){
+//    $img = "<img src='$file' width='150' />";
+//    echo $img;
+//}
+//else
+//{
+//echo "<img src='views/images/standard/_noproductimage.png' width='150' />";
+//}
 
 ?>
 	
