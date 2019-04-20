05055188c1ea53a5e39646fec3d445506282ad8d
monabhagwat <bhagwat.mona@gmail.com>
2019-04-20 07:37:09 +0100

    New 'about us' page and aboutusstyle css file

---
+++ views/hopefullyFinalAboutUsPage.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+         <title>About Us</title>
+    </head>
+    <body>
+        
+    <header class="white_bg text-center py-5 mb-4">
+        <div class="container">
+            <h1 class="font-weight-light text-pink">About Us</h1>
+        </div>
+    </header>
+        <link href="https://fonts.googleapis.com/css?family=Asap|Pacifico" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.1/flatly/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link href="AboutUsStyles.css" rel="stylesheet" type="text/css"/>
+<!-- Page Content -->
+
+<div class="circular">
+    
+    
+    <div class="column">
+        <img src="images/UNP_39609_Lloyds_Omolade Ojo004.png" alt="omo" style="width:50%">
+        <p>Hi Everyone! I’m Omo and I love theatre, food and reading.</p>
+    </div>
+    
+    
+    <div class="column">
+        <img src="images/gabby.png" alt="gaby" style="width:50%">
+        <p>I'm Gabriella, an English student at the University of Sussex. My writing engages with the cultural, historical, creative, performed, theoretical, and linguistic aspects of English as a world language and literature. Join me on a bold journey to reach a new level of understanding.</p>
+    </div>
+    
+    
+    <div class="column">
+        <img src="images/mona.png" alt="mona" style="width:50%">
+        <p> I am Mona, an Accountant by profession and a cook by passion. I am a dreamer, food enthusiast and a mother of two beautiful boys! Born in an Indian family of connoisseurs, experimenting with spices is embedded in my genes but my family’s love for new dishes has made me explore for recipes from around the world. My recipes are tried and tested in my kitchen, reflecting my life and my roots. Explore the world of Spices, Flavours, Gastronomy and much more along with me as I discover new recipes and global flavours.</p>
+    </div>
+    
+    
+    <div class="column">
+         <img src="images/jyoti.png" alt="jyoti" style="width:50%">
+         <p>Jyoti is a very creative person with excellence in styling and making things look pretty. She is an amazing mum to a beautiful girl and an expert in multitasking.  </p>  
+    </div>
+    
+    
+     <div class="column">
+         <img src="images/niki.png" alt="niki" style="width:50%">
+         <p> Hi. I’m Niki, and I welcome you in this corner of the internet where I share the practical methods I have developed in the past few years to create a life that I like living without compromising my introvert self. Join me on my journey to find creative and practical solutions for everyday problems like planning, procrastination, overwhelm and much more. </p>
+    </div>
+    
+    
+     <div class="column">
+         <img src="images/najwa.png" alt="najwa" style="width:50%">
+         <p>Najwa is a very happy and cheerful girl. She is second name to fashion and styling, with immense knowledge of latest trends, colour schemes and travel. She is well travelled and is an expert in designer brands.</p>
+    </div>
+</div>
+
+<div class="content">
+</div>
+    <footer id="myFooter">
+        <div class="container">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Comment</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Vlog</a></li>
+            </ul>
+        <p class="footer-copyright">© 2019 Copyright Honeycomb Bees Team</p>
+        </div>
+        <div class="footer-social">
+            <a href="#" class="social-icons"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="social-icons"><i class="fa fa-instagram"></i></a>
+            <a href="#" class="social-icons"><i class="fa fa-twitter"></i></a>
+        </div>
+    </footer>
+   
+  
+
+    </body>
+        
+        
+</html>        
+	
+
+
+
+
+
+
+
+
+
+
+
