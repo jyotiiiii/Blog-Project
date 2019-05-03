d9d91bd0b1d6c97eef63d923f2eee99e668ec091
Jyoti Sharma <jyoti.contact@gmail.com>
2019-05-03 21:56:59 +0100

    styling changes

--- views/pages/home.php
+++ views/pages/home.php
@@ -4,25 +4,37 @@
 
 <!-- Blog Posts and Images-->
 
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-light text-center">
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-white text-center">
         <div class="col-md-12 px-0">
          <!-- <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
           <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
           <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>-->
            <div class="social-icons clearfix"> 
                
-             <a href="index.php" onMouseOver="document.MyImage.src='https://i.imgur.com/QN3DDtC.png';" onMouseOut="document.MyImage.src='https://i.imgur.com/5LVRHtR.png';">
-	<img src="https://i.imgur.com/5LVRHtR.png" name="MyImage" height="173px" width="150px">
+<!--Inline javascript mouseover-->  
+<a href="#" id="fashion">
+    <img title="Najwa" src="https://i.imgur.com/5LVRHtR.png" onmouseover="this.src='https://i.imgur.com/QN3DDtC.png'" onmouseout="this.src='https://i.imgur.com/5LVRHtR.png'" height="173px" width="150px"/>
 </a>
+   
+<a href="#" id="lifestyle">
+    <img title="Niki" src="https://i.imgur.com/DziTvph.png" onmouseover="this.src='https://i.imgur.com/eFAolQa.png'" onmouseout="this.src='https://i.imgur.com/DziTvph.png'" height="173px" width="150px"/>
+</a>
+
+<a href="#" id="writing">
+    <img title="Gabriella" src="https://i.imgur.com/vvySn6n.png" onmouseover="this.src='https://i.imgur.com/fkfMJmw.png'" onmouseout="this.src='https://i.imgur.com/vvySn6n.png'" height="173px" width="150px"/>
+</a>
+
+
+
+               
           <a class=""><a href=""><img src="https://i.imgur.com/5LVRHtR.png" title="FASHION" height="173px" width="150px"/></a>                   
                   
           <a class=""><a href=""><img src="https://i.imgur.com/5LVRHtR.png" title="FASHION" height="173px" width="150px"/></a> 
                   
           <a class=""><a href=""><img src="https://i.imgur.com/5LVRHtR.png" title="FASHION" height="173px" width="150px"/></a>                   
                   
-          <a class=""><a href=""><img src="https://i.imgur.com/5LVRHtR.png" title="FASHION" height="173px" width="150px"/></a>                   
+                      
                   
-          <a class=""><a href=""><img src="https://i.imgur.com/5LVRHtR.png" title="FASHION" height="173px" width="150px"/></a>                   
         </div>
         </div>
       </div>
@@ -39,14 +51,14 @@
               <p class="card-text mb-auto">This is where the leading goes.</p>
               <a class="text-light bg-dark" href="#">VIEW BLOG</a>
             </div>
-              <img class="card-img-right flex-auto d-none d-lg-block" href="https://imgur.com/RSEiswj"><img src="https://i.imgur.com/RSEiswj.jpg" title="source: imgur.com" style="width: 370px; height: 370px;"/></a>
-            <!--<img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16a785f03f1%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16a785f03f1%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">href="https://imgur.com/RWhW18q"><img src="https://i.imgur.com/38DyfOO.png" title="source: imgur.com"-->
+              <!--<img class="card-img-right flex-auto d-none d-lg-block" href="https://imgur.com/RSEiswj"><img src="https://i.imgur.com/RSEiswj.jpg" title="source: imgur.com" style="width: 370px; height: 370px;"/></a>-->
+          <img class="card-img-right flex-auto d-none d-lg-block" href="https://imgur.com/RWhW18q"><img src="https://i.imgur.com/RWhW18q.png" title="source: imgur.com" style="width: 370px; height: 370px;"/></a>
           </div>
         </div>
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 shadow-sm h-md-370">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-black">Cooking</strong>
+              <strong class="d-inline-block mb-2 text-black">Food</strong>
               <h3 class="mb-0">
                 <a class="text-dark" href="#">Post title</a>
               </h3>
@@ -54,8 +66,7 @@
               <p class="card-text mb-auto">This is where the leading goes.</p>
               <a class="text-light bg-dark" href="#">VIEW BLOG</a>
             </div>
-              <img class="card-img-right flex-auto d-none d-lg-block" href="https://imgur.com/y6xQD2v"><img src="https://i.imgur.com/y6xQD2v.png" title="source: imgur.com" style="width: 370px; height: 370px;" /></a>
-            <!--<img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16a785f03f7%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16a785f03f7%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 200px; height: 250px;">-->
+              <img class="card-img-right flex-auto d-none d-lg-block" href="https://imgur.com/y6xQD2v"><img src="https://i.imgur.com/cORpz75.png" title="source: imgur.com" style="width: 370px; height: 370px;" /></a>
           </div>
         </div>
       </div>
@@ -95,7 +106,7 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 shadow-sm h-md-370">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-black">Printing</strong>
+              <strong class="d-inline-block mb-2 text-black">Making</strong>
               <h3 class="mb-0">
                 <a class="text-dark" href="#">Post title</a>
               </h3>
